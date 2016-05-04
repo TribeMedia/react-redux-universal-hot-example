cf37b06fa723e3ae3cb5e66cebd1dffc6ad9a54a
Travis James <travis@tribemedia.io>
2016-05-04 08:45:52 -0500

    test save survey track event to get code

--- src/containers/Survey/Survey.js
+++ src/containers/Survey/Survey.js
@@ -14,14 +14,12 @@ export default class Survey extends Component {
   }
 
   handleSubmit = (data) => {
-
-
-    //window.optimizely.push(['trackEvent', 'saveSurvey']);
-    request.get('https://5662157414.log.optimizely.com/event?a=5662157414&d=5662157414&y=false&src=js&s5692111775=gc&s5697131616=false&s5688002034=direct&tsent=1462367147.538&n=saveSurvey&u=oeu1462367115866r0.9819769772170281&wxhr=true&time=1462367147.538&f=5750250113&g=5824140674&cx2=3632d2e0')
+    window.optimizely.push(['trackEvent', 'saveSurvey']);
+    /*request.get('https://5662157414.log.optimizely.com/event?a=5662157414&d=5662157414&y=false&src=js&s5692111775=gc&s5697131616=false&s5688002034=direct&tsent=1462367147.538&n=saveSurvey&u=oeu1462367115866r0.9819769772170281&wxhr=true&time=1462367147.538&f=5750250113&g=5824140674&cx2=3632d2e0')
       .end(function(err, res) {
         window.alert('Data submitted! ' + JSON.stringify(data));
         this.props.initialize('survey', {});
-      });
+      });*/
   }
 
   handleInitialize = () => {
